cde1d94d60555bddc5c4b26270f631004dd33712
SerggioLysenko <serggio88888@gmail.com>
2019-08-04 12:16:09 +0300

    main activity,  layout added

---
+++ app/src/main/java/com/capitals/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.capitals
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+//    val countries = arrayListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
