6b054dd7765502c9fb8e4b3a62e46e3dea9f2345
SerggioLysenko <serggio88888@gmail.com>
2019-08-04 12:44:20 +0300

    commit2

--- app/src/main/java/com/capitals/MainActivity.kt
+++ app/src/main/java/com/capitals/MainActivity.kt
@@ -8,11 +8,27 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    val countries = arrayListOf<String>()
+    val countries = mapOf(
+        "Австралия" to "Канберра",
+        "Австрия" to "Вена",
+        "Азербайджан" to "Баку",
+        "Албания" to "Тирана",
+        "Алжир" to "Алжир",
+        "Ангола" to "Луанда",
+        "Андорра" to "Андорра - ла - Велья",
+        "Антигуа и Барбуда" to "Сент - Джонс",
+        "Аргентина" to "Буэнос - Айрес",
+        "Армения" to "Ереван",
+        "Аруба" to "Ораньестад",
+        "Афганистан" to "Кабул"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(countries.get("Австралия"))
+
 
 
     }
